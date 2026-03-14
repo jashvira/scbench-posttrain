762de720c71ecd60986db97322f842cf5253a8ff
jashvira <jashvira2001404@gmail.com>
2026-03-14 18:24:31 +1030

    Remove placeholder scaffold modules

--- src/scbench_posttrain/__init__.py
+++ src/scbench_posttrain/__init__.py
@@ -1,5 +1,1 @@
-"""SCBench post-training helpers."""
-
-from scbench_posttrain.tasks import TaskFamily, TaskInstance, TaskRegistry, VerificationResult
-
-__all__ = ["TaskFamily", "TaskInstance", "TaskRegistry", "VerificationResult"]
+"""scbench-posttrain."""
--- tests/test_imports.py
+++
@@ -1,6 +0,0 @@
-from scbench_posttrain import TaskRegistry
-
-
-def test_registry_smoke():
-    registry = TaskRegistry()
-    assert registry.names() == []
