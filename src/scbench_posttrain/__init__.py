d90fef8e0687b1d68cb6e0a51e900c2b246a6424
jashvira <jashvira2001404@gmail.com>
2026-03-14 13:38:04 +1030

    Add minimal post-training package scaffold

---
+++ src/scbench_posttrain/__init__.py
@@ -0,0 +1,5 @@
+"""SCBench post-training helpers."""
+
+from scbench_posttrain.tasks import TaskFamily, TaskInstance, TaskRegistry, VerificationResult
+
+__all__ = ["TaskFamily", "TaskInstance", "TaskRegistry", "VerificationResult"]
